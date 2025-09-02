b3d4c31dc222c4de6f78e0121ae295f9466e7448
Kevin Becker <kevin@kevinbecker.us>
2025-09-02 14:18:09 -0400

    Updated version
    And formatting

--- src/admin/realm-settings/SCIMTab.tsx
+++ src/admin/realm-settings/SCIMTab.tsx
@@ -19,10 +19,7 @@ import {
 } from "../../shared/@patternfly/react-core";
 import { FormProvider, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import {
-    FormPanel,
-    TextControl
-} from "../../shared/keycloak-ui-shared";
+import { FormPanel, TextControl } from "../../shared/keycloak-ui-shared";
 import { useAlerts } from "../../shared/keycloak-ui-shared";
 import { FormAccess } from "../components/form/FormAccess";
 import useToggle from "../utils/useToggle";
@@ -45,26 +42,32 @@ export const RealmSettingsSCIMTab = ({ realm, save }: RealmSettingsSCIMTabProps)
     const { handleSubmit, reset: resetForm, watch } = form;
 
     const reset = () => resetForm(realm);
-    const [isEnabled, _, setEnable] = useToggle(realm.attributes?.["scim.enabled"] === "true");
+    const [isEnabled, _, setEnable] = useToggle(
+        realm.attributes?.["scim.enabled"] === "true"
+    );
 
-    const watchAuthMode = watch(convertAttributeNameToForm("attributes.scim.authentication.mode"), "KEYCLOAK");
+    const watchAuthMode = watch(
+        convertAttributeNameToForm("attributes.scim.authentication.mode"),
+        "KEYCLOAK"
+    );
 
     const toggleKey = (key: string) => {
-        return key.startsWith("_")
-            ? key.substring(1)
-            : `_${key}`;
-    }
+        return key.startsWith("_") ? key.substring(1) : `_${key}`;
+    };
 
     const toggleScim = () => {
         const newIsEnabled = !isEnabled;
         // Toggle the state
         setEnable(newIsEnabled);
 
-        // If disabling, rename any realm SCIM attributes to _scim (so they 
+        // If disabling, rename any realm SCIM attributes to _scim (so they
         // are invalided by SCIM server)
         const attributes = { ...realm.attributes };
         for (const key in attributes) {
-            if (key.startsWith(newIsEnabled ? "_scim." : "scim") && !key.includes("enabled")) {
+            if (
+                key.startsWith(newIsEnabled ? "_scim." : "scim") &&
+                !key.includes("enabled")
+            ) {
                 attributes[toggleKey(key)] = attributes[key];
                 delete attributes[key];
             }
@@ -73,7 +76,7 @@ export const RealmSettingsSCIMTab = ({ realm, save }: RealmSettingsSCIMTabProps)
         // Set the new enabled attribute
         attributes["scim.enabled"] = newIsEnabled;
 
-        if(newIsEnabled && !("scim.authentication.mode" in attributes)) {
+        if (newIsEnabled && !("scim.authentication.mode" in attributes)) {
             attributes["scim.authentication.mode"] = "KEYCLOAK";
         }
 
@@ -81,11 +84,9 @@ export const RealmSettingsSCIMTab = ({ realm, save }: RealmSettingsSCIMTabProps)
 
         // Save this update
         save({ ...realm, attributes });
-    }
+    };
 
-    const saveForm = () => {
-
-    }
+    const saveForm = () => {};
 
     return (
         <PageSection variant="light">
@@ -121,37 +122,52 @@ export const RealmSettingsSCIMTab = ({ realm, save }: RealmSettingsSCIMTabProps)
                         <Switch
                             id="realm-scim-auth-mode-switch"
                             data-testid="realm-scim-auth-mode-switch"
-                            name={convertAttributeNameToForm("attributes.scim.authentication.mode")}
+                            name={convertAttributeNameToForm(
+                                "attributes.scim.authentication.mode"
+                            )}
                             label={t("scim.modes.keycloak")}
                             labelOff={t("scim.modes.external")}
                             className="pf-v5-u-mr-lg"
                             isDisabled={!isEnabled}
-                            defaultChecked={["KEYCLOAK", undefined].includes(realm.attributes?.["scim.authentication.mode"])}
+                            defaultChecked={["KEYCLOAK", undefined].includes(
+                                realm.attributes?.["scim.authentication.mode"]
+                            )}
                             aria-label={t("scim.modes.keycloak")}
                             onChange={(_event, value) => {
-                                form.setValue(convertAttributeNameToForm("attributes.scim.authentication.mode"), value ? "KEYCLOAK" : "EXTERNAL");
+                                form.setValue(
+                                    convertAttributeNameToForm(
+                                        "attributes.scim.authentication.mode"
+                                    ),
+                                    value ? "KEYCLOAK" : "EXTERNAL"
+                                );
                             }}
                         />
                         <TextControl
-                            name={convertAttributeNameToForm("attributes.scim.external.issuer")}
+                            name={convertAttributeNameToForm(
+                                "attributes.scim.external.issuer"
+                            )}
                             label={t("scim.external.issuer")}
                             labelIcon={t("scim.external.issuerHelp")}
                             placeholder={t("scim.external.issuerPlaceholder")}
-                            isDisabled={! (isEnabled && watchAuthMode === "EXTERNAL")}
+                            isDisabled={!(isEnabled && watchAuthMode === "EXTERNAL")}
                         />
                         <TextControl
-                            name={convertAttributeNameToForm("attributes.scim.external.jwks.uri")}
+                            name={convertAttributeNameToForm(
+                                "attributes.scim.external.jwks.uri"
+                            )}
                             label={t("scim.external.jwks.uri")}
                             labelIcon={t("scim.external.jwks.uriHelp")}
                             placeholder={t("scim.external.jwks.uriPlaceholder")}
-                            isDisabled={! (isEnabled && watchAuthMode === "EXTERNAL")}
+                            isDisabled={!(isEnabled && watchAuthMode === "EXTERNAL")}
                         />
                         <TextControl
-                            name={convertAttributeNameToForm("attributes.scim.external.audience")}
+                            name={convertAttributeNameToForm(
+                                "attributes.scim.external.audience"
+                            )}
                             label={t("scim.external.audience")}
                             labelIcon={t("scim.external.audienceHelp")}
                             placeholder={t("scim.external.audiencePlaceholder")}
-                            isDisabled={! (isEnabled && watchAuthMode === "EXTERNAL")}
+                            isDisabled={!(isEnabled && watchAuthMode === "EXTERNAL")}
                         />
 
                         <ActionGroup>
@@ -160,7 +176,7 @@ export const RealmSettingsSCIMTab = ({ realm, save }: RealmSettingsSCIMTabProps)
                                     variant="primary"
                                     type="submit"
                                     data-testid="scim-tab-save"
-                                    isDisabled={! isEnabled}
+                                    isDisabled={!isEnabled}
                                     onClick={saveForm}
                                 >
                                     {t("save")}
